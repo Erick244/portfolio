b1fc21f89b1ed81e520d3e464f4c6e09ac5897fc
Erick244 <erienrique03@gmail.com>
2024-02-14 16:49:18 -0300

    fetch data for Jorney

--- client/src/components/about/components/Jorney.tsx
+++ client/src/components/about/components/Jorney.tsx
@@ -1,49 +1,21 @@
 import { Achievement as AcheivementData } from "@/components/admin/management/jorney/table/JorneyTableColumns";
 import { H1 } from "@/components/shadcn-ui/typography/H1";
+import { getData } from "@/functions/api";
 import { cn } from "@/lib/utils";
 import { HTMLAttributes } from "react";
 import { Achievement } from "../templates/achievement";
 
-const jorneyDataTemp: AcheivementData[] = [
-    {
-        id: 1,
-        dateFormated: "January 24th, 2023",
-        title: "JavaScript Curse Initialized",
-        color: "#ff6105",
-    },
-    {
-        id: 2,
-        dateFormated: "December 24th, 2022",
-        title: "Git Curse Initialized",
-        color: "#d4d013",
-    },
-    {
-        id: 3,
-        dateFormated: "January 04th, 2024",
-        title: "Curse Initialized",
-        color: "#173ca3",
-    },
-    {
-        id: 4,
-        dateFormated: "January 24th, 2024",
-        title: "Java Financial",
-        color: "#e81cbf",
-    },
-    {
-        id: 5,
-        dateFormated: "November 30th, 2023",
-        title: "Curse Initialized",
-        color: "#198a0c",
-    },
-];
+export async function Jorney(props: HTMLAttributes<HTMLDivElement>) {
+    const jorney = await getData<AcheivementData[]>("/jorney", {
+        cache: "no-store",
+    });
 
-export function Jorney(props: HTMLAttributes<HTMLDivElement>) {
     return (
         <div className={cn("", props.className)}>
             <H1 className="text-center mb-10">Jorney</H1>
-            <div className="space-y-10 pb-10 overflow-y-scroll max-h-full">
-                {jorneyDataTemp &&
-                    jorneyDataTemp.map((achievement) => {
+            <div className="space-y-10 pb-10 overflow-y-scroll max-h-96 h-full">
+                {jorney &&
+                    jorney.map((achievement) => {
                         const isEven = achievement.id % 2 === 0;
 
                         return (
