739f83d20c13cb747bc3414e3e10c22509006e88
Erick244 <erienrique03@gmail.com>
2024-02-15 13:47:48 -0300

    changed from only backend image to card in project card

--- client/src/components/projects/templates/project/ProjectImage.tsx
+++ client/src/components/projects/templates/project/ProjectImage.tsx
@@ -1,4 +1,6 @@
+import { H3 } from "@/components/shadcn-ui/typography/H3";
 import { cn } from "@/lib/utils";
+import { Server } from "lucide-react";
 import Image from "next/image";
 import { HTMLAttributes } from "react";
 
@@ -15,13 +17,20 @@ export function ProjectImage({ imageUrl, ...props }: ProjectImageProps) {
                 props.className
             )}
         >
-            <Image
-                className="object-cover w-full h-full"
-                width={500}
-                height={500}
-                alt="Project image"
-                src={imageUrl || "/only-backend.png"}
-            />
+            {imageUrl ? (
+                <Image
+                    className="object-cover w-full h-full"
+                    width={500}
+                    height={500}
+                    alt="Project image"
+                    src={imageUrl}
+                />
+            ) : (
+                <div className="w-full rounded-lg h-full p-10 bg-clip-border border border-background flex gap-4 flex-col justify-center items-center bg-[linear-gradient(to_right,hsl(var(--background))_1px,transparent_1px),linear-gradient(to_bottom,hsl(var(--background))_1px,transparent_1px)] bg-[size:24px_24px] bg-background/20 text-foreground/60">
+                    <H3 className="text-3xl">ONLY BACKEND</H3>
+                    <Server className="w-10 h-10" />
+                </div>
+            )}
         </div>
     );
 }
