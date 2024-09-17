1a327256008ac2aae87572b8b5a3335d0bbc31ef
Erick244 <erienrique03@gmail.com>
2024-09-17 17:54:01 -0300

    feat(h2): add custom h2

---
+++ src/components/typography/H2.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
+
+interface H2Props extends HTMLAttributes<HTMLHeadingElement> {
+    children: React.ReactNode;
+}
+
+export function H2({ children, ...props }: H2Props) {
+    return (
+        <div className="relative w-full h-0.5 bg-foreground flex items-center justify-center my-10">
+            <h2
+                style={{
+                    color: "hsl(var(--background))",
+                }}
+                className={cn(
+                    "absolute scroll-m-20 text-3xl font-black text-shadow-md shadow-black/50 ",
+                    props.className
+                )}
+                {...props}
+            >
+                {children}
+            </h2>
+        </div>
+    );
+}
