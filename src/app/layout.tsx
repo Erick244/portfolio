fe7e6411341a5a6eea11a2fa6e2ec031539ad18e
Erick244 <erienrique03@gmail.com>
2024-09-17 18:02:16 -0300

    fix(layout): remove x scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <html lang="en">
             <body
                 className={cn(
-                    "min-h-screen w-screen antialiased bg-gradient-to-tr from-foreground from-0% to-background to-100%",
+                    "h-screen antialiased bg-gradient-to-tr from-foreground from-0% to-background to-100% ",
                     font.className
                 )}
             >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { H2 } from "@/components/typography/H2";
 
 export default function Home() {
     return (
-        <div className="min-h-screen  w-full flex flex-col items-center my-10">
+        <main className="h-full w-full flex flex-col items-center my-10 ">
             <Introduction />
             <H2>PROJECTS</H2>
-        </div>
+        </main>
     );
 }
