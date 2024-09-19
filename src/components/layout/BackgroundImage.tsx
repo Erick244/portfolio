8d2dab33824abc3aabfd6a1f3e67086cdc0e0b20
Erick244 <erienrique03@gmail.com>
2024-09-19 13:30:01 -0300

    fix(layout): background image takes up all the space

--- src/components/layout/BackgroundImage.tsx
+++ src/components/layout/BackgroundImage.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function BackgroundImage() {
     return (
         <Image
-            className="-z-10 absolute inset-0 w-full h-full"
+            className="-z-10 absolute inset-0 w-screen min-h-screen"
             alt="Abstract waves pattens background"
             src={"/images/bg-2.png"}
             width={400}
