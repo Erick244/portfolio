1ae9038bd7f20f6867673238561025bcee585b82
Erick244 <erienrique03@gmail.com>
2024-09-20 14:33:39 -0300

    style(ProjectCard): change the style

--- src/components/project-card/ProjectCardRoot.tsx
+++ src/components/project-card/ProjectCardRoot.tsx
@@ -21,16 +21,19 @@ export function ProjectCardRoot({
         setIsHover(!isHover);
     }
 
+    const customGradient = `linear-gradient(${
+        isEven ? "-30deg" : "30deg"
+    }, ${color} 0%, ${color} 40%, #000 40%,  ${color} 200%)`;
+
     return (
         <div
             {...props}
             onMouseEnter={toggleHover}
             onMouseLeave={toggleHover}
             style={{
-                background: `linear-gradient(${
-                    isEven ? "-30deg" : "30deg"
-                }, ${color} 0%, ${color} 40%, #ffffff46 40%, #ffffff46 100%)`,
-                backgroundSize: isHover ? "100% 200%" : "100% 100%",
+                background: customGradient,
+                backgroundSize: isHover ? "100% 150%" : "100% 100%",
+                backgroundPosition: isHover ? "% 200%" : "0% 0%",
             }}
             className={cn(
                 "relative flex flex-col md:flex-row gap-5 md:gap-10 even:md:flex-row-reverse items-center p-5  md:px-10 md:py-6 transition-all duration-200 hover:scale-105 border border-background/10 rounded-lg shadow-[0_0_10px] shadow-black/20 group",
