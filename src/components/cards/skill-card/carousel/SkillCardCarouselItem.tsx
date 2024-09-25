b5b8731be324614e00900b1ade83fb034ef9e2f9
Erick244 <erienrique03@gmail.com>
2024-09-25 15:39:51 -0300

    feat(SkillCardCarouselItem): add active prop

--- src/components/cards/skill-card/carousel/SkillCardCarouselItem.tsx
+++ src/components/cards/skill-card/carousel/SkillCardCarouselItem.tsx
@@ -1,16 +1,25 @@
 import { CarouselItem } from "@/components/ui/carousel";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 interface SkillCardCarouselItemProps {
     imageUrl: string;
     alt: string;
+    active?: boolean;
 }
 
-export function SkillCardItem({ imageUrl, alt }: SkillCardCarouselItemProps) {
+export function SkillCardItem({
+    imageUrl,
+    alt,
+    active,
+}: SkillCardCarouselItemProps) {
     return (
-        <CarouselItem className="select-none basis-1/4 hover:scale-90 transition-all duration-200 cursor-pointer">
+        <CarouselItem className=" select-none basis-1/4 hover:scale-90 transition-all duration-200 cursor-pointer">
             <Image
-                className="aspect-square w-full h-full max-w-[120px] max-h-[120px] rounded-lg"
+                className={cn(
+                    "aspect-square w-full h-full max-w-[120px] max-h-[120px] rounded-lg border-2 bg-background/10 p-1",
+                    active ? "border-background" : "border-transparent"
+                )}
                 alt={alt}
                 src={imageUrl}
                 width={100}
