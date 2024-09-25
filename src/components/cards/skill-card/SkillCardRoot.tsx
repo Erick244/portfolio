a05a338f50d1a69adf4aa560ad77c205d68492cd
Erick244 <erienrique03@gmail.com>
2024-09-25 16:34:53 -0300

    style(SkillCardRoot): add transition

--- src/components/cards/skill-card/SkillCardRoot.tsx
+++ src/components/cards/skill-card/SkillCardRoot.tsx
@@ -9,7 +9,7 @@ export function SkillCardRoot({ children, color }: SkillCardRootProps) {
             style={{
                 boxShadow: `0 10px 15px -3px ${color}, 0 4px 6px -4px ${color}`,
             }}
-            className="bg-foreground/50 shadow-lg py-5 px-10 rounded-lg w-[400px] sm:w-[500px] md:w-full md:max-w-3xl"
+            className="transition-all duration-500 bg-foreground/50 shadow-lg py-5 px-10 rounded-lg w-[400px] sm:w-[500px] md:w-full md:max-w-3xl"
         >
             {children}
         </div>
