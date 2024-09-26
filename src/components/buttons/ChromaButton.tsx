3dcf2313ae9772aeec88d7e639bc297aea8b5ff8
Erick244 <erienrique03@gmail.com>
2024-09-26 15:40:29 -0300

    refactor(ChromaButton): add support to pass ref

--- src/components/buttons/ChromaButton.tsx
+++ src/components/buttons/ChromaButton.tsx
@@ -1,18 +1,24 @@
 import { cn } from "@/lib/utils";
+import { forwardRef } from "react";
 import { Button, ButtonProps } from "../ui/button";
 
-export function ChromaButton({ children, ...props }: ButtonProps) {
-    return (
-        <Button
-            {...props}
-            size="icon"
-            className={cn(
-                "flex-shrink-0 shadow-md shadow-black/50 h-9 w-9 before:opacity-0 after:opacity-0 chroma-border hover:before:opacity-100 hover:after:opacity-100 ",
+export const ChromaButton = forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ children, ...props }, ref) => {
+        return (
+            <Button
+                {...props}
+                ref={ref}
+                size="icon"
+                className={cn(
+                    "flex-shrink-0 shadow-md shadow-black/50 h-9 w-9 before:opacity-0 after:opacity-0 chroma-border hover:before:opacity-100 hover:after:opacity-100 ",
 
-                props.className
-            )}
-        >
-            {children}
-        </Button>
-    );
-}
+                    props.className
+                )}
+            >
+                {children}
+            </Button>
+        );
+    }
+);
+
+ChromaButton.displayName = "ChromaButton";
