cc49bffc4e33ce7724ddaa0b6eeab5383c87adcd
Erick244 <erienrique03@gmail.com>
2024-10-01 17:04:52 -0300

    fix(ProjectCard): add again the size changing

--- src/components/cards/project-card/ProjectCardRoot.tsx
+++ src/components/cards/project-card/ProjectCardRoot.tsx
@@ -26,6 +26,7 @@ export function ProjectCardRoot({
             }}
             className={cn(
                 "relative flex flex-col md:flex-row gap-5 md:gap-10 even:md:flex-row-reverse items-center p-5  md:px-10 md:py-6 transition-all duration-200 hover:scale-105 border border-background/10 rounded-lg shadow-[0_0_10px] shadow-black/20 group",
+                isEven ? "md:flex-row-reverse" : "md:flex-row",
                 props.className
             )}
         >
